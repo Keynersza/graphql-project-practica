e832ea0dcb0880c8031c55ce0df86c3590f4516e
Keyner Salazar <alexsalazar210509@gmail.com>
2023-11-07 16:48:31 -0400

    upload new card

--- src/Views/Producto.jsx
+++ src/Views/Producto.jsx
@@ -3,32 +3,42 @@ import { useParams } from 'react-router-dom';
 import { useAllProducts } from '../components/Context/Products/customHooks-products';
 
 const Producto = () => {
-const [productoId, setProductoId] = useState([])
-const { loading, data, error } = useAllProducts();
-const { id } = useParams();
-console.log('id', id);
- const getProductsId = () => {
-    let datos = data.allProducts.filter((product) => product.id === id)
-    setProductoId(datos)
-    console.log("hola", datos);
-};
-useEffect(() => {
- getProductsId()
-}, [])
+  const [productoId, setProductoId] = useState([]);
+  const { loading, data, error } = useAllProducts();
+  const { id } = useParams();
+  console.log('id', id);
+  const getProductsId = () => {
+    let datos = data.allProducts.filter((product) => product.id === id);
+    setProductoId(datos);
+    console.log('hola', datos);
+  };
+  useEffect(() => {
+    getProductsId();
+  }, [productoId]);
 
-return(
- <h1>
-    {productoId.map((product) => (
-      <div>
-        <img src= {`data:image/png;base64${product.imageProduct}`}  />
-        <h1>Nombre: {product.nameProduct}</h1>
-        <h1>Descripcion: {product.description}</h1>
-        <h1>Precio: {product.price}</h1>
-        <h1>Due Date: {product.dueDate}</h1>
-      </div>
-    ))}
- </h1>   
-)
+  return (
+    <h1>
+      {loading ? (
+        loading
+      ) : (
+        <>
+          {productoId.map((product) => (
+            <div className="product-completee">
+              <img src={`data:image/png;base64${product.imageProduct}`} className='img-product-box'  />
+              <div className="box-productosss">
+                <h2 className="box-nameee">Nombre: {product.nameProduct}</h2>
+                <p className="box-descrippp">
+                  Descripcion: {product.description}
+                </p>
+                <h2 className="box-priceee">Precio: {product.price}</h2>
+                <p className="box-dateee">Due Date: {product.dueDate}</p>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
+    </h1>
+  );
 };
 
 export default Producto;
